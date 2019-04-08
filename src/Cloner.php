90889ae2ad7d48b59b694d6a58d1d3d177828752
Daniel Bachhuber <daniel@bachhuber.co>
2019-04-08 12:38:31 -0700

    Obscure repo URL when cloning, in case it contains secrets

--- src/Cloner.php
+++ src/Cloner.php
@@ -15,7 +15,7 @@ class Cloner {
 		$shorthash = substr( md5( mt_rand() . time() ), 0, 7 );
 		$repo_local_working_copy = sys_get_temp_dir() . '/composer-update-' . $shorthash;
 		$cmd = 'hub clone ' . escapeshellarg( $repo_url ) . ' ' . escapeshellarg( $repo_local_working_copy );
-		Logger::info( $cmd );
+		Logger::info( str_replace( escapeshellarg( $repo_url ), 'OBSCURED_REPO_URL', $cmd ) );
 		passthru( $cmd, $return_code );
 		if ( 0 !== $return_code ) {
 			Logger::error( 'Git failed to clone repository.' );
